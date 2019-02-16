290fac3c31fa4649b75f9206f869d4e7c1aff013
ethanzohar <ethanzohar9@gmail.com>
2019-02-16 11:04:27 -0500

    Thing

--- camera test.py
+++ camera test.py
@@ -5,15 +5,41 @@ from matplotlib import pyplot as plt
 cap = cv2.VideoCapture(0)
 
 while(True):
-    ret1, frame = cap.read()
+	ret1, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    ret2, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	ret2, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
-    cv2.imshow('frame', gray)
+	#noise removal
+	kernel = np.ones((3,3), np.uint8)
+	opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations = 3)
 
-    if (cv2.waitKey(1) & 0xFF == ord('q')):
-        break
+	# sure background area
+	sure_bg = cv2.dilate(opening,kernel,iterations=3)
+	# Finding sure foreground area
+	dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+	ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+
+	# Finding unknown region
+	sure_fg = np.uint8(sure_fg)
+	unknown = cv2.subtract(sure_bg,sure_fg)
+
+	# Marker labelling
+	ret, markers = cv2.connectedComponents(sure_fg)
+
+	# Add one to all labels so that sure background is not 0, but 1
+	markers = markers+1
+
+	# Now, mark the region of unknown with zero
+	markers[unknown==255] = 0
+
+	markers = cv2.watershed(frame,markers)
+	frame[markers == -1] = [255,0,0]
+
+	cv2.imshow('frame', frame)
+
+	if (cv2.waitKey(1) & 0xFF == ord('q')):
+		break
 
 cap.release()
 cv2.destroyAllWindows()
