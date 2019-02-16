28439d74f6ae0c38418eff8b1a10995e7e89fc65
ethanzohar <ethanzohar9@gmail.com>
2019-02-16 17:29:38 -0500

    David fix

--- twitter.py
+++ twitter.py
@@ -2,6 +2,8 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+from tweepy import Cursor
+from tweepy import API
 import json
 import re
 from watson_developer_cloud import ToneAnalyzerV3
@@ -25,6 +27,7 @@ consumer_key = "nqXHh0t7oJpGbdeGhp2PGmP81"
 consumer_secret = "bXcaZmYR1kqaWXNHLDKBBH6ngNkGkkAA3WhYTXJEaeRo8HMnmP"
 
 tweetString = ""
+tweetList = []
 tones = {}
 
 #This is a basic listener that just prints received tweets to stdout.
@@ -41,6 +44,7 @@ class StdOutListener(StreamListener):
 		if ('RT @' in tweet):
 			tweet = tweet.split(": ", 1)[1]
 
+		tweetList.append(tweet)
 		tweetString += tweet
 		self.counter += 1
 		if (self.counter < self.limit):
@@ -56,31 +60,38 @@ class StdOutListener(StreamListener):
 
 
 #This handles Twitter authetification and the connection to Twitter Streaming API
-l = StdOutListener()
+# l = StdOutListener()
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-stream = Stream(auth, l)
+# stream = Stream(auth, l)
 
 #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-if (len(sys.argv) > 1):
-	stream.filter(track=[sys.argv[1]])
-else:
-	stream.filter(track=['#maga'])
+# if (len(sys.argv) > 1):
+# 	stream.filter(track=[sys.argv[1]])
+# else:
+# 	stream.filter(track=['#maga'])
 
 
 
-# for text in tweetList:
-tone_analysis = tone_analyzer.tone(
-	{'text': tweetString},
-	'application/json'
-).get_result()
+# # for text in tweetList:
+# tone_analysis = tone_analyzer.tone(
+# 	{'text': tweetString},
+# 	'application/json'
+# ).get_result()
 
-tweetSentences = json.loads(json.dumps(tone_analysis, indent=2)).get("sentences_tone")
+# tweetSentences = json.loads(json.dumps(tone_analysis, indent=2)).get("sentences_tone")
 
-for sentence in tweetSentences:
-	for tone in sentence.get("tones"):
-		tone = tone.get("tone_id")
-		tones[tone] = tones.get(tone, 0) + 1
+# for sentence in tweetSentences:
+# 	for tone in sentence.get("tones"):
+# 		tone = tone.get("tone_id")
+# 		tones[tone] = tones.get(tone, 0) + 1
 
-print(tones)
-sys.stdout.flush()
+# print(tones)
+# sys.stdout.flush()
+
+api = API(auth)
+
+query = 'python'
+max_tweets = 1
+searched_tweets = [status for status in Cursor(api.search, q=query).items(max_tweets)]
+print(searched_tweets)
