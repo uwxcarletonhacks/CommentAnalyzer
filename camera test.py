ae51e581ce5316b38b483f1b369c3103b282104a
ethanzohar <ethanzohar9@gmail.com>
2019-02-16 10:51:38 -0500

    New branch

--- camera test.py
+++ camera test.py
@@ -1,14 +1,17 @@
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
 cap = cv2.VideoCapture(0)
 
 while(True):
-    ret, frame = cap.read()
+    ret1, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret2, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
     cv2.imshow('frame', gray)
+
     if (cv2.waitKey(1) & 0xFF == ord('q')):
         break
 
